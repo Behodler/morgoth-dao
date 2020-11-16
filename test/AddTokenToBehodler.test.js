be332df7e3613437aca1faddaf86f9b0fb6d48f6
Justin Goro <justingoro@protonmail.com>
2020-11-16 13:40:06 +0200

    added empty tests

---
+++ test/AddTokenToBehodler.test.js
@@ -0,0 +1,15 @@
+const { accounts, contract } = require('@openzeppelin/test-environment');
+const { expectEvent, expectRevert, ether } = require('@openzeppelin/test-helpers');
+const { expect, assert } = require('chai');
+const { BNtoBigInt } = require('./helpers/BigIntUtil');
+const bigNum = require('./helpers/BigIntUtil')
+
+describe('AddTokenToBehodler', async function () {
+    const [owner, secondary] = accounts;
+
+    beforeEach(async function () {})
+
+    it('test1',async function (){
+
+    })
+})
